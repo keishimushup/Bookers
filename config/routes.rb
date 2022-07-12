d07c7de377057601070d7c481904cca7e2091dc5
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-07-12 10:05:43 +0000

    [update]投稿内容の保存機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'books/index'
-  get 'books/show'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
   get 'books/edit'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
