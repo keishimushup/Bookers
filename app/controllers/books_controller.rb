0f14d98bf1d6e505d3b6fde6b0a5ade9349fb16c
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-07-14 04:49:41 +0000

    [Update]3バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
-    
+
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -18,13 +22,13 @@ class BooksController < ApplicationController
   def edit
     @book =Book.find(params[:id])
   end
-  
+
   def update
     book =Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
